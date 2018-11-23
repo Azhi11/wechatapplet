71648037063ec532aa437e013a3504032d2a4428
chenzhi <13509622742@163.com>
2018-11-23 19:03:27 +0800

    tttt

---
+++ application/index/controller/car.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/11/23
+ * Time: 18:40
+ */
+namespace app\index\controller;
+class Car
+{
+    public function test()
+    {
+        echo 'This is a test';
+    }
+}
